61802dfddc0bdb205c132c02ee608c01563b46cd
atomic <baileyweb04@gmail.com>
2024-03-09 21:16:26 -0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,20 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   },
   mode: 'development',
   entry: './src/index.js',
