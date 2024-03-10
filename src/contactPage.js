2a0f7e11d36b9701a1b71f834f72aa50c4f92b54
atomic <baileyweb04@gmail.com>
2024-03-09 21:04:25 -0500

    test

--- src/contactPage.js
+++ src/contactPage.js
@@ -1,14 +1,18 @@
 function contactPage() {
   const content = document.querySelector('#content');
   const contactHeader = document.createElement('h1');
+  const contactPara = document.createElement('p');
   const contactDiv = document.createElement('div');
+  const contactBtn = document.createElement('button');
   
   contactDiv.setAttribute("id", "contactdiv");
 
-  contactHeader.innerText = 'Contact us'
+  contactHeader.innerText = 'Contact us';
+  contactPara.innerText = 'Email: Example@example.com\nPhone: 555-555\nFacebook: @Example\nWebsite: example.com';
+  contactBtn.textContent = 'Contact us';
 
-  contactDiv.append(contactHeader)
-  content.appendChild(contactDiv)
+  contactDiv.append(contactHeader, contactPara, contactBtn);
+  content.appendChild(contactDiv);
 }
 
 export default contactPage;
