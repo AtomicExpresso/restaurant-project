3674584edec9015c34af36ac8ef268213cdbc2df
atomic <baileyweb04@gmail.com>
2024-03-09 19:01:47 -0500

    test

--- src/main.js
+++ src/main.js
@@ -1,15 +1,43 @@
 function main(){
+  //hero section
   const content = document.querySelector('#content')
   const heroDiv = document.createElement('div');
   let heroHeading = document.createElement('h1');
   let heroPara = document.createElement('p');
+  const button = document.createElement('button');
 
+  heroDiv.setAttribute("id", "main");
   heroHeading.innerText = 'Tony\'s Pizza';
   heroPara.innerText = 'Fresh baked pizza';
+  button.textContent = 'View Menu';
+
+  //About section
+  const aboutDiv = document.createElement('div');
+  const aboutTextDiv = document.createElement('div');
+  const aboutSubText = document.createElement('h4');
+  const aboutHeading = document.createElement('h1');
+  const aboutPara = document.createElement('p');
+  const aboutImg = document.createElement('img');
+
+  aboutDiv.setAttribute("id", "about");
+  aboutTextDiv.setAttribute("id", "abouttext");
+  aboutHeading.innerText = 'What makes our pizza great'
+  aboutPara.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+  aboutImg.src = '/images/about-img.png';
+  aboutSubText.innerText = 'Traditional';
 
   heroDiv.appendChild(heroHeading);
   heroDiv.appendChild(heroPara);
+  heroDiv.appendChild(button);
+
+  aboutTextDiv.appendChild(aboutSubText);
+  aboutTextDiv.appendChild(aboutHeading);
+  aboutTextDiv.appendChild(aboutPara);
+  aboutDiv.appendChild(aboutTextDiv);
+  aboutDiv.appendChild(aboutImg);
+
   content.appendChild(heroDiv);
+  content.appendChild(aboutDiv);
 }
 
 export default main
